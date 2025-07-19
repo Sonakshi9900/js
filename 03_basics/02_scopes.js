c627f3f9f5992374567c65aeeecaf1acd48e9535
Sonakshi Gupta <sonakshikumari9900@gmail.com>
2025-07-19 13:04:10 +0000

    scopesTalk

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,48 @@ if (true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-for(let i=0; i< Array.length; i++){
-    const element = array[i];
-}
+// for(let i=0; i< Array.length; i++){
+//     const element = array[i];
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "Sonakshi"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username = "Sonakshi"
+    if(username === "Sonakshi"){
+        const website = " youtube"
+       // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//+++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
