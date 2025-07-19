471c84685bfe506c3c8ea0df254598c3c1fb237b
Sonakshi Gupta <sonakshikumari9900@gmail.com>
2025-07-19 09:46:02 +0000

    functionss

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,35 @@ const result = addTwoNum(3 , 5)
  }
 
 //  console.log(loginUserMsg("Sonakshi"))
-console.log(loginUserMsg())
+//  console.log(loginUserMsg())
+
+function calculateCartPrice(val1 , val2,...num1){  // ... -> rest operator spread operator
+    return num1
+}
+
+console.log(calculateCartPrice(200 , 400 , 500 , 2000))
+
+const user = {
+    username: "Sonakshi",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price : 399
+})
+
+const myNewArray = [200 , 400 , 100 , 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 , 400 , 500 , 1000]));
+
